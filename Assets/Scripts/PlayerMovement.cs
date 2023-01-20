05a55c1a27a9bd6b8cae60826b57fa6cc0245c7e
kititnik <kititnik@gmail.com>
2023-01-20 20:14:37 +0200

    Player movement + sprite sorter

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private Vector2 direction;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody2D.MovePosition(_rigidbody2D.position + direction * (speed * Time.fixedDeltaTime));
+    }
+}
---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    public bool isStatic;
+    [SerializeField] private float offset;
+    private int _sortingOrderBase = 0;
+    private Renderer _renderer;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    private void LateUpdate()
+    {
+        _renderer.sortingOrder = (int)(_sortingOrderBase - transform.position.y + offset);
+        if (isStatic)
+            Destroy(this);
+    }
+}
