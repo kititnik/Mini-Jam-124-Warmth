2dca4d5d571cdf75545b6f01c699371d99f2e173
kititnik <kititnik@gmail.com>
2023-01-20 21:31:13 +0200

    Make logs spawner

--- Assets/Scripts/CollectLogs.cs
+++ Assets/Scripts/CollectLogs.cs
@@ -1,17 +1,16 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectLogs : MonoBehaviour
 {
     public int logsCount;
     [SerializeField] private string logsTag;
+    [SerializeField] private LogsSpawner logsSpawner;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag(logsTag)) return;
         logsCount++;
         Destroy(col.gameObject);
+        logsSpawner.StartLogsSpawn();
     }
 }
